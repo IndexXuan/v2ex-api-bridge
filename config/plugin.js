8b0a79a9be7c02a7ca4c1399fb4005d905e2e991
IndexXuan <indexxuan@gmail.com>
2017-03-27 15:52:53 +0800

    fix ci & package.json\' dep

---
+++ config/plugin.js
@@ -0,0 +1,11 @@
+/**
+ *  @Plugin
+ *  ---------------------------------------------
+ *  Author : IndexXuan(https://github.com/IndexXuan)
+ *  Mail   : indexxuan@gmail.com
+ *  Date   : Mon 27 Mar 2017 03:50:45 PM CST
+ */
+
+exports.validate = {
+  package: 'egg-validate',
+}
